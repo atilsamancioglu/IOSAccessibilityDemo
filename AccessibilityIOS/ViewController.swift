28f0391fc7eb1f3eb9c1197334936c7f631c6308
Atıl Samancıoğlu <samancioglu.atil@gmail.com>
2021-10-16 10:50:52 +0300

    Initial Commit

---
+++ AccessibilityIOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AccessibilityIOS
+//
+//  Created by Atil Samancioglu on 16.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
